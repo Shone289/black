e94d4b9c09ace9c60c081b4f34a40bbf150a98f9
Shone <vulicnebojsa994@gmail.com>
2022-02-05 12:24:55 +0100

    COMMITED

--- newJSFile.js
+++ newJSFile.js
@@ -1,1 +1,11 @@
 console.log("newJSFIle.js");
+var me = "Shone";
+
+function myf(name){
+  alert("Hello " + name);
+}
+
+myf(me);
+
+var ndiv = document.createElement("div");
+ndiv.innerHTML = "Content Inside div element";
